d44f76a3ab0cf78c9ef4c7ac3ccf1ebe52003d29
Joe <joe.vandelay@gmail.com>
2015-07-17 18:40:03 +0200

    infinite ajax loading of image stacks uses lazy img loading to optimize display time on mobile

--- js/jquery.jTinder.js
+++ js/jquery.jTinder.js
@@ -12,6 +12,7 @@
 		defaults = {
 			onDislike: null,
 			onLike: null,
+			onImageLoading: null,
 			beforeNextLoaded: null,
 			onNextLoaded: null,
 			animationRevertSpeed: 200,
@@ -21,7 +22,7 @@
 			likeSelector: '.like',
 			dislikeSelector: '.dislike',
 			nextSelector: null,
-			itemSeletor: '.infinite-item'
+			itemSelector: '.infinite-item'
 		};
 
 	function Plugin(element, options) {
@@ -37,11 +38,11 @@
 		init: function (element) {
 			this.container = $(element).find("ul");
 			this.noMoreData = false;
-			this.panes = $(this.container.find("li:visible"));
+			this.panes = $(this.container.find(this.settings.itemSelector));
 			this.index = this.panes.length; // faster if we just manage it ourselves and not recalculate evey next()
 			this.nextUrl = this.fetchNextUrl($); // fetchNextUrl from current document
-			this.current_pane = $(this.panes.last());
-			this.pane_width = this.current_pane.width();
+			this.currentPane = $(this.panes.last());
+			this.paneWidth = this.currentPane.width();
 			this.touchStart = false;
 			this.xStart = 0;
 			this.yStart = 0;
@@ -50,6 +51,16 @@
 			this.lastPosY = 0;
 			this.posX = 0;
 			this.posY = 0;
+			
+			/*var self=this;
+			this.panes.find("img").each(function(){
+				self.switchLazyImage($(this));
+			});*/
+			this.switchLazyImage(this.currentPane.find("img").first(), true);
+			this.currentPane.show();
+			this.prepareNext();
+
+
 
 			$(element).bind('touchstart mousedown', $.proxy(this.handler, this));
 			$(element).bind('touchmove mousemove', $.proxy(this.handler, this));
@@ -57,41 +68,97 @@
 		},
 
 		next: function () {
-			likePane.animate({'opacity': 0}, 1000);
-			dislikePane.animate({'opacity': 0}, 1000);
-			this.current_pane.animate({'opacity': 0}, 1000);
 
-			return this.hideTopPane();
+			this.hideTopPane() // visually
+			.done(function(){
+				this.currentPane.remove();
+				this.moveToNext()
+			}.bind(this));// done
+		},
+		// returns promise for animate effects
+		hideTopPane: function () {
+
+			/*likePane.animate({'opacity': 0}, 1000);
+			dislikePane.animate({'opacity': 0}, 1000);*/
+			return this.currentPane.animate({'opacity': 0}, 500).promise()
 		},
 
-	    loadNext: function()
+		moveToNext: function ()
+		{
+			if(this.index <=  this.settings.loadNextThreshold)
+			{
+				this.loadAjaxData();
+				return
+			}
+
+			this.index--;			
+			this.currentPane = this.panes.eq(this.index);
+			this.prepareNext();
+		},
+
+		prepareNext: function(pane)
+		{
+			pane = (pane)?(pane):(this.currentPane);
+			var $next = pane.prev();
+			this.switchLazyImage($next.find("img").first(), true);
+			$next.show();
+		},
+
+		switchLazyImage: function($img, forceVisible)
+		{		
+			$img = $img ? $img : this.currentPane.find("img").first();
+			if(!forceVisible && !$img.is("[lazySrc]")) // not lazy yet
+			{   
+			   switchAttribute($img,"src","lazySrc");
+			}
+			else // switch back
+			{
+				if($img.is("[lazySrc]")) // make visible if there a lazy			
+					switchAttribute($img,"lazySrc","src");
+				if(this.settings.onImageLoading) // callback
+	    			this.settings.onImageLoading($img, this);
+			}			 
+
+		},
+
+	    loadAjaxData: function()
 	    {
 	    	if(this.noMoreData)
+	    	{
+	    	 	if(this.settings.onNoMoreData)
+	    	 		this.settings.onNoMoreData();
 	    		return;
+	    	}
 
-	    	if(!this.nextUrl)  // first time or last time
-	    		this.noMoreData = True;
+	    	if(!this.nextUrl) {  // first time or last time
+	    		this.noMoreData = true; }
 
-			if(this.settings.beforeNextLoaded)
-				this.settings.beforeNextLoaded(this)
+			if(this.settings.beforeNextLoaded) {
+				this.settings.beforeNextLoaded(this)}
 
 
-			$.ajax({url: this.nextUrl, context: this}).done(function(data,textStatus,jqXHR)
+			$.ajax({url: this.nextUrl, context: this, async: true}).done(function(data,textStatus,jqXHR)
 			{
 				var $items = $(data).find(this.settings.itemSelector);
 
 				this.container.prepend($items);
-                this.panes = $items.parent().find("li:visible");
+                this.panes = this.container.find(this.settings.itemSelector);
+                var self = this;
+                /*this.panes.find("img").each(function(){
+                	 var $this = $(this);
+                	 self.switchLazyImage($this);
+                })*/
 
 				this.index = this.panes.length-1;// combine old and new items
-				this.current_pane = $(this.panes.eq(this.index));
+				this.currentPane = $(this.panes.eq(this.index));
+				this.switchLazyImage(this.currentPane.find("img").first(), true); // switch back the active one
+				this.prepareNext(); // an preload next
+				this.currentPane.show();
 
 				var oldUrl = this.nextUrl;
 				this.nextUrl = this.fetchNextUrl($(data));
 				if(!this.nextUrl || this.nextUrl == oldUrl)
 					noMoreData = true;
-
-				this.clearUnused();
 
 				if(this.settings.onNextLoaded) // callback
 	    			this.settings.onNextLoaded($items, this);
@@ -99,18 +166,7 @@
 			});
 	    },
 
-		hideTopPane: function () {
-
-			this.panes.eq(this.index).hide();
-			if(this.index <=  this.settings.loadNextThreshold)
-			{
-				this.loadNext();
-				return
-			}
-
-			this.index--;
-			this.current_pane = this.panes.eq(this.index);
-		},
+		
 
 		fetchNextUrl: function($context) {
 			var link;
@@ -118,20 +174,16 @@
 			return (link.length > 0)? (link[0].href):(null);
 		},
 
-		clearUnused: function () {
-			$(this.container.find("li:hidden")).remove();
-		},
-
 		dislike: function() {
 			if(this.settings.onDislike) {
-				this.settings.onDislike(this.current_pane);
+				this.settings.onDislike(this.currentPane);
 			}
 			this.next();
 		},
 
 		like: function() {
 			if(this.settings.onLike) {
-				this.settings.onLike(this.current_pane);
+				this.settings.onLike(this.currentPane);
 			}
 			this.next();
 		},
@@ -191,21 +243,23 @@
 						var pageY = typeof ev.pageY == 'undefined' ? ev.originalEvent.touches[0].pageY : ev.pageY;
 						var delta = this.calcPos(pageX, pageY);
 
-						var percent = delta.x * 100 / this.pane_width;
+						var percent = delta.x * 100 / this.paneWidth;
 
 						var opa = this.calcOpacity(delta);
 						if (opa >= 0.2)
 							ev.preventDefault();
 
-						likePane = this.current_pane.find(this.settings.likeSelector);
-						dislikePane = this.current_pane.find(this.settings.dislikeSelector);
+						likePane = this.currentPane.find(this.settings.likeSelector);
+						dislikePane = this.currentPane.find(this.settings.dislikeSelector);
+
+						//this.currentPane.css('opacity', 1-opa); // makes currentPane also opaque depending on delta
 
 						if (this.posX >= 0) {
-							this.current_pane.css('transform', "translate(" + this.posX + "px, 0px) rotate(" + (percent / 2) + "deg)");
+							//this.currentPane.css('transform', "translate(" + this.posX + "px, 0px) rotate(" + (percent / 2) + "deg)");
 							likePane.css('opacity', opa);
 							dislikePane.css('opacity', 0);
 						} else if (this.posX < 0) {
-							this.current_pane.css('transform', "translate(" + this.posX + "px, 0px) rotate(" + (percent / 2) + "deg)");
+							//this.currentPane.css('transform', "translate(" + this.posX + "px, 0px) rotate(" + (percent / 2) + "deg)");
 							dislikePane.css('opacity', opa);
 							likePane.css('opacity', 0);
 						}
@@ -234,10 +288,16 @@
 			} else {
 				this.lastPosX = 0;
 				this.lastPosY = 0;
-				this.current_pane.find(this.settings.likeSelector).animate({"opacity": 0, "transform": "translate(0px,0px) rotate(0deg)"}, this.settings.animationRevertSpeed);
-				this.current_pane.find(this.settings.dislikeSelector).animate({"opacity": 0, "transform": "translate(0px,0px) rotate(0deg)"}, this.settings.animationRevertSpeed);
+				this.currentPane.find(this.settings.likeSelector).animate({"opacity": 0, "transform": "translate(0px,0px) rotate(0deg)"}, this.settings.animationRevertSpeed);
+				this.currentPane.find(this.settings.dislikeSelector).animate({"opacity": 0, "transform": "translate(0px,0px) rotate(0deg)"}, this.settings.animationRevertSpeed);
 			}
 		}
+	};
+
+	function switchAttribute($elem, attr, repl)
+	{
+	 	$elem.attr(repl,$elem.attr(attr));
+        $elem.removeAttr(attr);	
 	};
 
 	$.fn[ pluginName ] = function (options) {
